7fcec94b173de62e46fda776bcc390ed9e28184a
Alexander Hopgood <alex.hopgood@gmail.com>
2016-02-03 08:37:50 +0000

    Provided an optional command line argument to the GetDilbert script.

--- dilbert/GetDilbert.py
+++ dilbert/GetDilbert.py
@@ -1,3 +1,7 @@
 import dilbert
+import sys
+import getopt
 
-dilbert.getDilbert(None)
+date = getopt.getopt(sys.argv,  "-d:")[1][1]
+print "Attempting to use date[",date,"]"
+dilbert.getDilbert(date)
---
+++ dilbert/backfill.py
@@ -0,0 +1,6 @@
+import dilbert
+
+
+while date > "2016-01-01":
+	date = date - 1
+	dilbert.getDilbert(date)
